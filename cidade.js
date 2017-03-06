0ec100d1a07f5bfad98cefc307f6334e4b9393df
thassiov <tvmcarvalho@gmail.com>
2017-03-06 14:37:46 -0300

    Adicionando perguntador

--- cidade.js
+++ cidade.js
@@ -5,3 +5,15 @@
 
 'use strict';
 
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Entre com o nome (ou parde do nome) da cidade: \n', function(res) {
+    console.log('res');
+    console.log(res);
+    rl.close();
+});
