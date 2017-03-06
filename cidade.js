05dec508a3fff5f5ee0d6914ac8b2eb95a128018
thassiov <tvmcarvalho@gmail.com>
2017-03-06 14:56:34 -0300

    Fazendo request para a API

--- cidade.js
+++ cidade.js
@@ -6,14 +6,28 @@
 'use strict';
 
 var readline = require('readline');
+var http = require('http');
+
+var API = {
+    host: 'http://api.teleport.org',
+    path: '/api/cities/?search='
+};
 
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Entre com o nome (ou parde do nome) da cidade: \n', function(res) {
-    console.log('res');
-    console.log(res);
+rl.question('Entre com o nome (ou parte do nome) da cidade: \n', function(ans) {
     rl.close();
+    http.get(API.host + API.path + ans.toLowerCase().trim(), function(res) {
+        var finalData = '';
+        res.on('data', function(d) {
+            finalData +=d;
+        });
+        res.on('end', function() {
+            finalData = JSON.parse(finalData);
+            console.log(JSON.stringify(finalData, null, 2));
+        });
+    });
 });
